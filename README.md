514207824d389eebb329519fa73ed051d11068f3
alecmocatta <alec@mocatta.net>
2018-07-20 12:58:01 +0100

    Initial commit. Only fallback hashing is working; LC_UUID, build-id etc todo

---
+++ README.md
@@ -0,0 +1,49 @@
+# build_id
+
+[![Crates.io](https://img.shields.io/crates/v/build_id.svg?style=flat-square&maxAge=86400)](https://crates.io/crates/build_id)
+[![Apache-2.0 licensed](https://img.shields.io/crates/l/build_id.svg?style=flat-square&maxAge=2592000)](LICENSE.txt)
+[![Build Status](https://ci.appveyor.com/api/projects/status/github/alecmocatta/build_id?branch=master&svg=true)](https://ci.appveyor.com/project/alecmocatta/build-id)
+[![Build Status](https://circleci.com/gh/alecmocatta/build_id/tree/master.svg?style=shield)](https://circleci.com/gh/alecmocatta/build_id)
+[![Build Status](https://travis-ci.com/alecmocatta/build_id.svg?branch=master)](https://travis-ci.com/alecmocatta/build_id)
+
+[Docs](https://docs.rs/build_id/0.1.0)
+
+Obtain a [`Uuid`](https://docs.rs/uuid/0.6/uuid/) uniquely representing the
+build of the current binary.
+
+This is intended to be used to check that different processes are indeed
+invocations of identically laid out binaries.
+
+As such:
+* It is guaranteed to be identical within multiple invocations of the same
+binary.
+* It is guaranteed to be different across binaries with different code or data
+segments or layout.
+* Equality is unspecified if the binaries have identical code and data segments
+and layout but differ immaterially (e.g. if a timestamp is included in the
+binary at compile time).
+
+# Examples
+
+```rust
+let local_build_id = build_id::get();
+if local_build_id == remote_build_id {
+	println!("We're running the same binary as remote!");
+} else {
+	println!("We're running a different binary to remote");
+}
+```
+
+# Note
+
+This looks first for linker-inserted build ID / binary UUIDs (i.e.
+`.note.gnu.build-id` on Linux; `LC_UUID` in Mach-O; etc), falling back to
+hashing the whole binary.
+
+## License
+Licensed under Apache License, Version 2.0, ([LICENSE.txt](LICENSE.txt) or
+http://www.apache.org/licenses/LICENSE-2.0).
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+licensed as above, without any additional terms or conditions.
---
+++ src/lib.rs
@@ -0,0 +1,128 @@
+//! Obtain a [Uuid] uniquely representing the build of the current binary.
+//!
+//! This is intended to be used to check that different processes are indeed
+//! invocations of identically laid out binaries.
+//!
+//! As such:
+//! * It is guaranteed to be identical within multiple invocations of the same
+//! binary.
+//! * It is guaranteed to be different across binaries with different code or
+//! data segments or layout.
+//! * Equality is unspecified if the binaries have identical code and data
+//! segments and layout but differ immaterially (e.g. if a timestamp is included
+//! in the binary at compile time).
+//!
+//! # Examples
+//!
+//! ```
+//! # let remote_build_id = build_id::get();
+//! let local_build_id = build_id::get();
+//! if local_build_id == remote_build_id {
+//! 	println!("We're running the same binary as remote!");
+//! } else {
+//! 	println!("We're running a different binary to remote");
+//! }
+//! ```
+//!
+//! # Note
+//!
+//! This looks first for linker-inserted build ID / binary UUIDs (i.e.
+//! `.note.gnu.build-id` on Linux; `LC_UUID` in Mach-O; etc), falling back to
+//! hashing the whole binary.
+
+#![doc(html_root_url = "https://docs.rs/build_id/0.1.0")]
+#![deny(missing_docs, warnings, deprecated)]
+#![allow(intra_doc_link_resolution_failure)]
+
+extern crate byteorder;
+extern crate twox_hash;
+extern crate uuid;
+
+use std::{env, fs, hash::Hasher, io, sync};
+use uuid::Uuid;
+
+static mut BUILD_ID: Uuid = Uuid::nil();
+static INIT: sync::Once = sync::ONCE_INIT;
+
+/// Returns a [Uuid] uniquely representing the build of the current binary.
+///
+/// This is intended to be used to check that different processes are indeed
+/// invocations of identically laid out binaries.
+///
+/// As such:
+/// * It is guaranteed to be identical within multiple invocations of the same
+/// binary.
+/// * It is guaranteed to be different across binaries with different code or
+/// data segments or layout.
+/// * Equality is unspecified if the binaries have identical code and data
+/// segments and layout but differ immaterially (e.g. if a timestamp is included
+/// in the binary at compile time).
+///
+/// # Examples
+///
+/// ```
+/// # let remote_build_id = build_id::get();
+/// let local_build_id = build_id::get();
+/// if local_build_id == remote_build_id {
+/// 	println!("We're running the same binary as remote!");
+/// } else {
+/// 	println!("We're running a different binary to remote");
+/// }
+/// ```
+///
+/// # Note
+///
+/// This looks first for linker-inserted build ID / binary UUIDs (i.e.
+/// `.note.gnu.build-id` on Linux; `LC_UUID` in Mach-O; etc), falling back to
+/// hashing the whole binary.
+#[inline]
+pub fn get() -> Uuid {
+	unsafe {
+		INIT.call_once(|| {
+			BUILD_ID = calculate();
+		});
+		BUILD_ID
+	}
+}
+fn calculate() -> Uuid {
+	// LC_UUID https://opensource.apple.com/source/libsecurity_codesigning/libsecurity_codesigning-55037.6/lib/machorep.cpp https://stackoverflow.com/questions/10119700/how-to-get-mach-o-uuid-of-a-running-process
+	// .note.gnu.build-id https://github.com/golang/go/issues/21564 https://github.com/golang/go/blob/178307c3a72a9da3d731fecf354630761d6b246c/src/cmd/go/internal/buildid/buildid.go
+	let file = fs::File::open(env::current_exe().unwrap()).unwrap();
+	let mut hasher = twox_hash::XxHash::with_seed(0);
+	io::copy(&mut &file, &mut HashWriter(&mut hasher)).unwrap();
+	let mut bytes = [0; 16];
+	<byteorder::LittleEndian as byteorder::ByteOrder>::write_u64(&mut bytes, hasher.finish());
+	Uuid::from_random_bytes(bytes)
+}
+
+struct HashWriter<T: Hasher>(T);
+impl<T: Hasher> io::Write for HashWriter<T> {
+	fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+		self.0.write(buf);
+		Ok(buf.len())
+	}
+	fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
+		self.write(buf).map(|_| ())
+	}
+	fn flush(&mut self) -> io::Result<()> {
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod test {
+	#[test]
+	fn brute() {
+		let x = super::calculate();
+		for _ in 0..1000 {
+			assert_eq!(x, super::calculate());
+		}
+	}
+	#[test]
+	fn get() {
+		let x = super::calculate();
+		assert_eq!(x, super::get());
+		assert_eq!(x, super::get());
+		assert_eq!(x, super::get());
+	}
+}
