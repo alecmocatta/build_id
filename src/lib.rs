9ace0e13b4d31d9cd45a3a98292ba7ef98aa6c18
Sean Bruton <sbruton@gmail.com>
2022-08-31 01:47:27 -0500

    update for uuid v1

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,9 @@
 
 use once_cell::sync::Lazy;
 use std::{
-	any::TypeId, hash::{Hash, Hasher}, io
+	any::TypeId,
+	hash::{Hash, Hasher},
+	io,
 };
 use uuid::Uuid;
 
@@ -137,9 +139,9 @@ fn calculate() -> Uuid {
 	<byteorder::NativeEndian as byteorder::ByteOrder>::write_u64(&mut bytes[8..], hasher.finish());
 
 	uuid::Builder::from_bytes(bytes)
-		.set_variant(uuid::Variant::RFC4122)
-		.set_version(uuid::Version::Random)
-		.build()
+		.with_variant(uuid::Variant::RFC4122)
+		.with_version(uuid::Version::Random)
+		.into_uuid()
 }
 
 struct HashWriter<T: Hasher>(T);
